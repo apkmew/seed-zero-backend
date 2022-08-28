f4d61a1bc7fbbd45f6a940c50daf2605cf085cd6
Apimook Tratree <57552159+apkmew@users.noreply.github.com>
2022-08-28 11:02:29 +0700

    Finish work

--- main.py
+++ main.py
@@ -11,29 +11,35 @@ NAME = os.getenv('NAME', default="World")
 app = FastAPI()
 
 # TODO: 1.) Add endpoint as a top-level page.
-@app.get("")
+@app.get("/")
 def index():
     return {"message": f"Hello {NAME}"}
 
 # TODO: 2.) Create a calculator that accept 2 query params and manipulate those 2 values.
-@app.get("/calculator/add")
+@app.get("/calculator/add/")
 def adder(first_number: int, second_number: int):
     # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
+    return {"first_number": first_number, "second_number": second_number, "result": first_number + second_number}
 
 @app.get("/calculator/substract")
 def subtractor(first_number: int, second_number: int):
     # TODO: Start here
-    return {"first_number": first_number, "second_number": second_number, "result": 0}
+    return {"first_number": first_number, "second_number": second_number, "result": first_number - second_number}
 
 # TODO: (Optional) @app.get("/calculator/multiply")
+@app.get("/calculator/multiply")
+def multiplier(first_number: int, second_number: int):
+    return {"first_number": first_number, "second_number": second_number, "result": first_number * second_number}
 
 # TODO: (Optional) @app.get("/calculator/divide")
+@app.get("/calculator/divide")
+def divider(first_number: int, second_number: int):
+    return {"first_number": first_number, "second_number": second_number, "result": first_number / second_number}
 
 
 
 # TODO: 3.) Enter a path scheme that accept userId as a path parameter `userId`.
-@app.get("")
+@app.get("/user/{userId}/")
 def usersInfo(userId: int):
     if userId != 9997:
         raise HTTPException(status_code=404, detail="UserId not found.")
@@ -52,6 +58,7 @@ def usersInfo(userId: int):
 #   "price": 123,
 # }
 # @app.????(???)
+@app.post("/item/create/")
 def itemCreator(item: Item):
     # TODO: Print body's name and price to console.
     return item.name, item.price
